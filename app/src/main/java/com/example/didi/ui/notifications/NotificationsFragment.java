553b5f59300e23e3e1a4d77397b402701bf1fa73
zzl <86543402@qq.com>
2019-12-28 01:19:06 +0800

    update

---
+++ app/src/main/java/com/example/didi/ui/notifications/NotificationsFragment.java
@@ -0,0 +1,56 @@
+package com.example.didi.ui.notifications;
+
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.didi.R;
+
+public class NotificationsFragment extends Fragment {
+
+    private NotificationsViewModel mViewModel;
+
+    public static NotificationsFragment newInstance() {
+        return new NotificationsFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        mViewModel =
+                ViewModelProviders.of(this).get(NotificationsViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+        final TextView textView = root.findViewById(R.id.text);
+        mViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return inflater.inflate(R.layout.fragment_notifications, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(NotificationsViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
